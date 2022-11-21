e67777341a9ef7dc154cc2fc0406da569fa8d6f6
Michael Chan <mijoch@gmail.com>
2022-11-20 20:17:36 -0800

    improve Button test coverage to 100%

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -39,3 +39,9 @@ Small.args = {
   size: 'small',
   label: 'Button',
 };
+
+export const BackgroundColor = Template.bind({});
+BackgroundColor.args = {
+  backgroundColor: "#C0FFEE",
+  label: 'Button',
+};
