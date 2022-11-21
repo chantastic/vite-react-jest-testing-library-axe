f29c384479c73d5495e31c405e657be06ff278fc
Michael Chan <mijoch@gmail.com>
2022-11-20 21:03:30 -0800

    add automated accessibility tests and a11y tree snapshot tests

--- .storybook/test-runner.cjs
+++ .storybook/test-runner.cjs
@@ -1,5 +1,23 @@
+const { injectAxe, checkA11y } = require("axe-playwright");
+
 module.exports = {
+  async preRender(page) {
+    await injectAxe(page);
+  },
   async postRender(page) {
+    // automate accessibility Jest snapshot tests
+    await checkA11y(page, "#root", {
+      detailedReport: true,
+      detailedReportOptions: {
+        html: true,
+      },
+    });
+
+    const accessibilityTree =
+      await page.accessibility.snapshot();
+    expect(accessibilityTree).toMatchSnapshot();
+
+    // automate Jest snapshot tests
     const elementHandler = await page.$("#root");
     const innerHTML = await elementHandler.innerHTML();
     expect(innerHTML).toMatchSnapshot();
